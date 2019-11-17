a67b48a2f7f4d2d34a394620ca0323e535d01feb
Badreddine Sayah <badreddine.sayah@nuance.com>
2019-11-17 08:57:36 -0500

    init hidden for encoder, build decoder 2 layer lstm

--- baseline.py
+++ baseline.py
@@ -17,6 +17,20 @@ class Encoder(nn.Module):
         return hid, cell
 
     # TODO init hidden state
-    # # TODO test the encoder
+    # TODO test the encoder
+    def init_hidden(self):
+        return torch.zeros(1, 1, self.hidden_size)
+
+# DECODER
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embedding_size, hidden_size, layers):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.layers = layers
+        # TODO add dropout
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.lstm = nn.LSTM(embedding_size, hidden_size, layers)
+        self.output = nn.Linear(hidden_size, vocab_size)
 
 
