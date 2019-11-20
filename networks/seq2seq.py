429b8d91e8df9243eb8e838c8b89aab824e30853
a-coles <arlie.coles@mail.mcgill.ca>
2019-11-20 07:50:07 -0500

    Vanilla Seq2Seq w/ padding + class structures

--- networks/seq2seq.py
+++ networks/seq2seq.py
@@ -1,30 +1,115 @@
 # -*- coding: utf-8 -*-
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
+class Seq2Seq():
+    def __init__(self, vocab, cfg, device):
+        self.device = device
+        self.encoder = Encoder(vocab.num_words, cfg['embedding_size'], cfg['hidden_size'], device).to(device)
+        self.decoder = Decoder(cfg['hidden_size'], cfg['embedding_size'], vocab.num_words, device).to(device)
+        self.vocab = vocab
+        self.name = 'seq2seq'
+
+        # Logging variables
+        self.train_losses, self.valid_losses = [], []
+        self.train_bleu, self.valid_bleu = [], []
+
+    def log_learning_curves(self):
+        '''
+        Logs the learning curve info to a csv.
+        TODO: update with BLEU.
+        '''
+        header = 'epoch,train_loss,valid_loss'
+        num_epochs = len(self.train_losses)
+        with open(os.path.join('log', '{0}_learning_curves.log'.format(self.name)), 'w') as fp:
+            fp.write('{0}\n'.format(header))
+            for e in range(num_epochs):
+                fp.write('{0},{1}\n'.format(e, self.train_losses[e]))#, self.valid_losses[e]))
+
+    def train(self, train_loader, loss_fn=None, train_bsz=1, num_epochs=1):
+        enc_opt = torch.optim.Adam(self.encoder.parameters())
+        dec_opt = torch.optim.Adam(self.decoder.parameters())
+        for epoch in range(num_epochs):
+            train_loss = self.train_epoch(train_loader, loss_fn, enc_opt, dec_opt, train_bsz)
+            print('EPOCH {0} \t train_loss {1}'.format(epoch, train_loss))
+            self.train_losses.append(train_loss)
+
+    def train_epoch(self, train_loader, loss_fn, enc_opt, dec_opt, train_bsz=1):
+        self.encoder.train()
+        self.decoder.train()
+        loss_epoch = 0.0
+        for i, (x, y, x_len, y_len) in enumerate(train_loader):
+            if i > 100:
+                break
+            enc_opt.zero_grad()
+            dec_opt.zero_grad()
+            inp_qc = self.vocab.get_sentence(x)
+            print('inp qc', ' '.join(inp_qc[0]))
+            inp_fr = self.vocab.get_sentence(y)
+            print('inp_fr', ' '.join(inp_fr[0]))
+
+            x, y = x.to(self.device), y.to(self.device)
+            enc_hid = self.encoder.init_hidden(train_bsz).to(self.device)
+
+            tgt_len = y.size(1)
+            loss = 0
+
+            # To store decoder outputs
+            outputs = torch.zeros(tgt_len, train_bsz, self.vocab.num_words).to(self.device)
+
+            # Whole sequence through encoder
+            enc_out, enc_hid = self.encoder(x, x_len, enc_hid)
+
+            # First input to the decoder is BOS (hardcoded: idx is 1)
+            dec_inp = torch.ones(train_bsz, device=self.device) * 1
+            dec_hid = enc_hid  # First decoder hidden state is last encoder hidden state
+
+            # One token at a time from decoder
+            for di in range(1, tgt_len):
+                dec_out, dec_hid = self.decoder(dec_inp, dec_hid)
+                outputs[di] = dec_out
+                tok = dec_out.argmax(1)
+                # No teacher forcing: next input is current output
+                dec_inp = tok
+
+            # When calculating loss, collapse batches together and
+            # remove leading BOS (since we feed this to everything)
+            all_outputs = outputs[1:].view(-1, outputs.shape[-1])
+            all_y = torch.transpose(y[1:].reshape(-1), 1, 0)
+            loss = loss_fn(all_outputs, all_y.long())
+
+            loss.backward()
+            pred_tok = torch.transpose(torch.argmax(outputs.detach(), dim=2), 1, 0)
+            print(' '.join(self.vocab.get_sentence(pred_tok.cpu())[0]))
+
+            enc_opt.step()
+            dec_opt.step()
+            loss_batch = loss.item() / tgt_len
+            loss_epoch += loss_batch
+        return loss_epoch
+
+
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size, device):
+    def __init__(self, input_size, embedding_size, hidden_size, device):
         super(Encoder, self).__init__()
         self.device = device
         self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.gru = nn.GRU(embedding_size, hidden_size)
 
     def forward(self, x, x_lens, hidden):
         x = x.long()
-        # print('enc inp', x.size())
-        embedded = self.embedding(x).unsqueeze(0)
-        # Ignore the padding during backprop
+        embedded = self.embedding(x)
+        # Ignore the padding through the RNN
         embedded = torch.nn.utils.rnn.pack_padded_sequence(embedded, x_lens,
                                                            batch_first=True,
                                                            enforce_sorted=False)
-        # print('enc emb', embedded.size())
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        # print('enc out', output.size())
-        # output, output_len = torch.nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+        output, hidden = self.gru(embedded, hidden)
+        # Re-pad
+        output, output_len = torch.nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
         return output, hidden
 
     def init_hidden(self, batch_size):
@@ -32,36 +117,21 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size, device):
+    def __init__(self, hidden_size, embedding_size, output_size, device):
         super(Decoder, self).__init__()
         self.device = device
         self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.embedding = nn.Embedding(output_size, embedding_size)
+        self.gru = nn.GRU(embedding_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x, hidden):
-        x = x.long()
-        # print('dec inp', x.size())
-        output = self.embedding(x).unsqueeze(0)  #.view(1, 1, -1)  # TODO: ?
-        # print('dec emb', output.size())
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))  # ?
+        # Input here is always one token at a time,
+        # so need to do some unsqueezing to account for length dimension (1)
+        x = x.long().unsqueeze(0)
+        embedded = self.embedding(x)#.permute(0, 2, 1)#.unsqueeze(0)  # TODO: ?
+        embedded = F.relu(embedded)
+        output, hidden = self.gru(embedded, hidden)
+        output = self.out(output.squeeze(0))
         return output, hidden
-
-    # def init_hidden(self):
-    #     return torch.zeros(1, 1, self.hidden_size)
-
-
-# class Seq2Seq(nn.Module):
-#     def __init__():
-#         super(Seq2Seq, self).__init__()
-#         self.encoder = Encoder()
-#         self.decoder = Decoder()
-
-#     def forward(self, x):
-#         x = self.encoder(x)
-#         x = self.decoder(x)
-#         return x
--- train_seq2seq.py
+++ train_seq2seq.py
@@ -11,8 +11,7 @@ from assemble_corpus import Vocab
 from dataset import QcFrDataset, pad_collate
 from torch.utils.data import DataLoader
 
-#from baseline import Encoder, Decoder, Seq2Seq
-from networks.seq2seq import Encoder, Decoder
+from networks.seq2seq import Seq2Seq
 
 
 if __name__ == '__main__':
@@ -39,102 +38,7 @@ if __name__ == '__main__':
     valid_loader = DataLoader(valid_dataset, batch_size=cfg['valid_bsz'], shuffle=True)
 
     # Training loop
-    encoder = Encoder(vocab.num_words, cfg['hidden_size'], device).to(device)
-    decoder = Decoder(cfg['hidden_size'], vocab.num_words, device).to(device)
-    enc_optimizer = optim.Adam(encoder.parameters())
-    dec_optimizer = optim.Adam(decoder.parameters())
-    criterion = nn.NLLLoss()
-
-    for epoch in range(cfg['epochs']):
-        encoder.train()
-        decoder.train()
-        loss_epoch = 0
-
-        for i, (qc_idx, fr_idx, qc_len, fr_len) in enumerate(train_loader):
-            if i > 10:
-                break
-            enc_hid = encoder.init_hidden(cfg['train_bsz']).to(device)
-            enc_optimizer.zero_grad()
-            dec_optimizer.zero_grad()
-            inp_qc = vocab.get_sentence(qc_idx)
-            print('inp qc', ' '.join(inp_qc[0]))
-            inp_fr = vocab.get_sentence(fr_idx)
-            print('inp_fr', ' '.join(inp_fr[0]))
-
-            qc_idx, fr_idx = qc_idx.to(device), fr_idx.to(device)
-
-            inp_len = qc_idx.size(1)
-            tgt_len = fr_idx.size(1)
-            enc_outputs = torch.zeros(100, encoder.hidden_size, device=device)
-            loss = 0
-
-            for ei in range(inp_len):
-                # print(qc_idx[:, ei].size())
-                enc_out, enc_hid = encoder(qc_idx[:, ei], qc_len, enc_hid)
-                print(enc_out)
-                enc_outputs[ei] = enc_out[0, 0]
-
-            dec_inp = torch.ones(cfg['train_bsz'], device=device) * 1
-            dec_hid = enc_hid  # First decoder hidden state is last encoder hidden state
-            for di in range(tgt_len):
-                dec_out, dec_hid = decoder(dec_inp, dec_hid)
-
-                # print(dec_out.type(), fr_idx[:, di].type())
-                loss += criterion(dec_out, fr_idx[:, di].long())
-                # if dec_out.item() == torch.tensor([[2]], device=device):  # ??? TODO: no hardcode for EOS
-                #     break
-                print(vocab.idx2word[str(torch.argmax(dec_out.detach()).item())])
-
-            loss.backward(retain_graph=True)
-            enc_optimizer.step()
-            dec_optimizer.step()
-            loss_batch = loss.item() / tgt_len
-            loss_epoch += loss_batch
-        print(loss_epoch)
-
-
-
-    # initialization
-    # encoder = Encoder(vocab.num_words, cfg['embedding_size'], cfg['hidden_size'], cfg['num_layers'])
-    # decoder = Decoder(vocab.num_words, cfg['embedding_size'], cfg['hidden_size'], cfg['num_layers'])
-
-    # seq2seq = Seq2Seq(encoder, decoder, vocab.num_words, device)
-    # model = seq2seq.to(device)
-    # optimizer = optim.Adam(model.parameters())
-    # criterion = nn.CrossEntropyLoss()
-    # for epoch in range(cfg['epochs']):
-    #     model.train()
-    #     total_loss = 0
-    #     # TODO consider teacher forcing, gradient clipping,
-    #     for idx, (qc_idx, fr_idx) in enumerate(train_loader):
-    #         loss = 0
-    #         # print(qc_idx)
-    #         inp_qc = vocab.get_sentence(qc_idx)
-    #         print(inp_qc)
-    #         optimizer.zero_grad()
-    #         qc_idx, fr_idx = qc_idx.to(device), fr_idx.to(device)
-    #         print(qc_idx.type(), fr_idx.type())
-    #         out = model(qc_idx, fr_idx)
-    #         #out = model(qc_idx)
-    #         # TODO reshape tensors
-    #         out, fr_idx = out.float(), fr_idx.long()
-    #         print(out.type(), fr_idx.type())
-
-    #         print('-----')
-    #         print(out, out.size())
-    #         print(fr_idx, out.size())
-
-    #         for i in range(out.size()[0]):
-    #             print(i)
-    #             print(out[idx, i], fr_idx[idx, i])
-    #             loss += criterion(out[:,i], fr_idx[:,i])
-    #         #loss = criterion(out, fr_idx)
-    #         # break
-    #         loss.backward()
-    #         optimizer.step()
-    #         total_loss += loss.item()
-    #         break
-        
-    #     total_loss = total_loss/len(train_loader)
-    #     print('total loss', total_loss)
-
+    criterion = nn.CrossEntropyLoss(ignore_index=vocab.word2idx['PAD'])
+    model = Seq2Seq(vocab, cfg, device)
+    model.train(train_loader, loss_fn=criterion, train_bsz=cfg['train_bsz'], num_epochs=cfg['num_epochs'])
+    model.log_learning_curves()
